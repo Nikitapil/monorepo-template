5c3fb7bd0fde1dbf3f33109c3ca31f1a72450fc0
Nikitapil <nikitapiliavets@gmail.com>
2023-12-12 21:33:05 +0300

    add shared styles

--- services/moduleA/src/components/App/App.tsx
+++ services/moduleA/src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import styles from './App.module.scss';
 
 export const App = () => {
   return (
     <div data-testid="App">
-      <h1>MODULE A</h1>
+      <h1 className={styles.title}>MODULE A</h1>
       <Outlet />
     </div>
   );
